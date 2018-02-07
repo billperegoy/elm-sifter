dec6e163f9c5e1c54c0acf5eb37cff8743c07855
Bill Peregoy <bill@peregoy.org>
2018-02-06 19:44:06 -0500

    Added ability to match across all extractors (#4)

--- src/Sifter.elm
+++ src/Sifter.elm
@@ -3,24 +3,72 @@ module Sifter exposing (..)
 import Regex
 
 
-matchOne : a -> (a -> String) -> String -> ( Bool, a )
-matchOne elem extractor string =
+type SortOrder
+    = Ascending
+    | Descending
+
+
+type ConjunctionType
+    = And
+    | Or
+
+
+type alias SortFields a =
+    { fields : List (Extractor a)
+    , order : SortOrder
+    }
+
+
+type alias Config a =
+    { extractors : List (Extractor a)
+    , limit : Int
+    , sort : SortFields a
+    , filter : Bool
+    , conjunction : ConjunctionType
+    , respectWordBoundaries : Bool
+    }
+
+
+type alias Extractor a =
+    a -> String
+
+
+reducer : List ( Bool, a ) -> Bool
+reducer list =
+    List.foldl (\e accum -> accum || Tuple.first (e)) False list
+
+
+matchAll : String -> a -> List (Extractor a) -> ( Bool, a )
+matchAll string elem extractors =
+    let
+        hasMatch =
+            List.map (\e -> matchOne e string elem) extractors
+                |> reducer
+    in
+        ( hasMatch, elem )
+
+
+matchOne : Extractor a -> String -> a -> ( Bool, a )
+matchOne extractor string elem =
     let
         matcher =
             string
                 |> Regex.regex
                 |> Regex.caseInsensitive
+
+        matchResult =
+            Regex.contains matcher (extractor elem)
     in
-        ( Regex.contains matcher (extractor elem), elem )
+        ( matchResult, elem )
 
 
-sifter : List a -> (a -> String) -> String -> List a
+sifter : List a -> Config a -> String -> List a
 sifter data config string =
     let
         matcher =
             Regex.regex (string)
     in
         data
-            |> List.map (\elem -> matchOne elem config string)
+            |> List.map (\e -> matchAll string e config.extractors)
             |> List.filter Tuple.first
             |> List.map Tuple.second
