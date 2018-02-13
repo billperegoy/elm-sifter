24b338041401d6a4465a36534cf36a7ffc5daa39
Bill Peregoy <bill@gnar.dog>
2018-02-12 21:54:12 -0500

    More work on the demo app

--- src/Main.elm
+++ src/Main.elm
@@ -27,15 +27,19 @@ type alias Place =
 
 
 type alias Model =
-    { inputText : String
+    { limit : String
+    , inputText : String
     , places : List Place
+    , config : Sifter.Config Place
     }
 
 
 init : Model
 init =
-    { inputText = ""
+    { limit = "10"
+    , inputText = ""
     , places = Places.all
+    , config = config
     }
 
 
@@ -45,6 +49,7 @@ init =
 
 type Msg
     = SetInputText String
+    | SetLimit String
 
 
 update : Msg -> Model -> Model
@@ -52,6 +57,19 @@ update msg model =
     case msg of
         SetInputText text ->
             { model | inputText = text }
+
+        SetLimit text ->
+            let
+                limit =
+                    text |> String.toInt |> Result.withDefault 0
+
+                old_config =
+                    model.config
+
+                config =
+                    { old_config | limit = limit }
+            in
+                { model | limit = text, config = config }
 
 
 
@@ -62,10 +80,13 @@ view : Model -> Html Msg
 view model =
     let
         places =
-            filteredPlaces model.inputText model.places
+            filteredPlaces model.config model.inputText model.places
     in
         div []
-            [ input [ onInput SetInputText ] []
+            [ label [] [ text "limit" ]
+            , input [ onInput SetLimit ] []
+            , showConfig model.config
+            , input [ onInput SetInputText ] []
             , ul []
                 (List.map
                     (\e -> li [] [ text (e.city ++ ", " ++ e.stateAbbrev) ])
@@ -74,10 +95,86 @@ view model =
             ]
 
 
+extractorsString : Sifter.Config Place -> String
+extractorsString config =
+    let
+        extractorStrings =
+            config.extractors
+                |> List.map (\x -> getExtractorName x)
+                |> String.join (", ")
+    in
+        "    { extractors = [ " ++ extractorStrings ++ " ]\n"
+
+
+limitString : Sifter.Config Place -> String
+limitString config =
+    "    , limit = " ++ toString config.limit ++ "\n"
+
+
+conjunctionString : Sifter.Config Place -> String
+conjunctionString config =
+    "    , conjunction = " ++ toString config.conjunction ++ "\n"
+
+
+respectWordBoundariesString : Sifter.Config Place -> String
+respectWordBoundariesString config =
+    "    , respectWordBoundaries = " ++ toString config.respectWordBoundaries ++ "\n"
+
+
+sortString : Sifter.Config Place -> String
+sortString config =
+    case config.sort of
+        Nothing ->
+            "    , sort = Nothing\n"
+
+        Just sort ->
+            "    , sort = Just { "
+                ++ "field = "
+                ++ getExtractorName sort.field
+                ++ ", order = "
+                ++ toString sort.order
+                ++ " }\n"
+
+
+filterString : Sifter.Config Place -> String
+filterString config =
+    "    , filter = " ++ toString config.filter ++ "\n"
+
+
+getExtractorName : Extractor Place -> String
+getExtractorName extractor =
+    let
+        place =
+            { city = ".city"
+            , stateAbbrev = ".stateAbbrev"
+            , state = ".state"
+            }
+    in
+        extractor place
+
+
+showConfig : Sifter.Config Place -> Html Msg
+showConfig config =
+    pre []
+        [ code []
+            [ text
+                ("    config =\n"
+                    ++ extractorsString config
+                    ++ limitString config
+                    ++ conjunctionString config
+                    ++ respectWordBoundariesString config
+                    ++ sortString config
+                    ++ filterString config
+                    ++ "    }\n"
+                )
+            ]
+        ]
+
+
 config : Sifter.Config Place
 config =
     { extractors = [ .city, .stateAbbrev, .state ]
-    , limit = 15
+    , limit = 10
     , sort =
         Just
             { field = .city
@@ -89,6 +186,6 @@ config =
     }
 
 
-filteredPlaces : String -> List Place -> List Place
-filteredPlaces string places =
+filteredPlaces : Sifter.Config Place -> String -> List Place -> List Place
+filteredPlaces config string places =
     Sifter.sifter config string places
