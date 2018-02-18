ed15e8b854464575fda00fefe8e39ddb2892dad9
Bill Peregoy <bill@gnar.dog>
2018-02-17 21:24:45 -0500

    Added documentation

--- src/Sifter.elm
+++ src/Sifter.elm
@@ -9,6 +9,10 @@ module Sifter
         , ConjunctionType(..)
         )
 
+{-| This library is intended to be used with autocompete. A user cab supply a list
+of arbitrary Elm structs and a config describing what criteria to use to sift the data.
+-}
+
 import Regex
 
 
